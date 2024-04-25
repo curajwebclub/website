26972d8d747907d971354e2936bf140bf1ca1636
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-04-25 23:41:40 +0530

    Removing unrequired files

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var typed = new Typed(".typing", {
-        strings: ["CWC","CURAJ WEB CLUB"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-});
