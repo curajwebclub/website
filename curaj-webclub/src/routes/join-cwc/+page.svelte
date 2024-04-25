fe0cf36781787f98d12c06694a9f4874602e568f
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-04-25 23:27:47 +0530

    Upgrading to sveltekit

---
+++ curaj-webclub/src/routes/join-cwc/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import '$lib/style.css';
+
+	import _$ from 'jquery';
+	import Typed from 'typed.js';
+	_$(document).ready(function () {
+		var typed = new Typed('.typing', {
+			strings: ['CWC', 'CURAJ WEB CLUB'],
+			typeSpeed: 100,
+			backSpeed: 60,
+			loop: true
+		});
+	});
+</script>
+
+<svelte:head>
+	<title>Join CWC</title>
+</svelte:head>
+<section class="onboard" id="onboard">
+	<div class="max-width">
+		<div class="onboard-content">
+			<div class="text-2">Coming Soon</div>
+
+			<a href="/">← Go Back</a>
+		</div>
+	</div>
+</section>
